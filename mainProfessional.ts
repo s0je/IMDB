804fbb302ad217c17bd99a2dd877abe69fe1256d
s0je <cuentasegurajss@gmail.com>
2022-01-23 12:57:38 +0100

    Primera actualizacion

---
+++ mainProfessional.ts
@@ -0,0 +1,8 @@
+import { Professional } from "./professional";
+
+let DiCaprio = new Professional ("Leonar Dicaprio", 47, "Masculino", 183, 75, "castaño", "caucasico", false, "americano", 7, "Actor");
+let Denzel = new   Professional ("Denzel Washington", 67, "Masculino", 185, 80, "negro", "afroamericano", false, "americano", 2, "Actor");
+let Almodovar = new Professional ("Pedro Almodovar",72, "Masculino", 177, 85, "blanco", "caucasico", false, "español", 2, "Director");
+
+
+DiCaprio.printAll();
